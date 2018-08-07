2ae639fb218bfa90aa22a41c0543af083b140d78
nanawel <nanawel@gmail.com>
2018-08-07 13:31:37 +0200

    Add factory for AMD/Requirejs loading

--- src/zmultiselect.js
+++ src/zmultiselect.js
@@ -31,7 +31,39 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 !*/
 
-(function ($) {
+(function( factory ) {
+    "use strict";
+
+    if ( typeof define === 'function' && define.amd ) {
+        // AMD
+        define( ['jquery'], function ( $ ) {
+            return factory( $, window, document );
+        } );
+    }
+    else if ( typeof exports === 'object' ) {
+        // CommonJS
+        module.exports = function (root, $) {
+            if ( ! root ) {
+                // CommonJS environments without a window global must pass a
+                // root. This will give an error otherwise
+                root = window;
+            }
+
+            if ( ! $ ) {
+                $ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
+                    require('jquery') :
+                    require('jquery')( root );
+            }
+
+            return factory( $, root, root.document );
+        };
+    }
+    else {
+        // Browser
+        factory( jQuery, window, document );
+    }
+}
+(function ($, window, document) {
     "use strict";
 
     var clickEvent = "click";
@@ -550,4 +582,4 @@ THE SOFTWARE.
     };
 
 
-})(jQuery);
+}));
